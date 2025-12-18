6cf60c8bcd653fe26959f3a78609819b0e3f7f78
Dio Naufal Vianto <dionaufalvi@gmail.com>
2025-12-18 12:11:53 +0700

    modified slot_machine.py, fixed the betting system

--- Day 4/slot_machine.py
+++ Day 4/slot_machine.py
@@ -4,46 +4,52 @@ from collections import Counter
 
 def spin():
     spinned_items = [random.choice(("🍉","🍇","🍊","🍌")) for item in range(3)]
-    
+    return spinned_items
+
+def display(spin_result):
     print("=====================")
-    for item in spinned_items:
-        
+    for item in spin_result:
         print(f"| {item} |",end=" ")
     print(" ")
     print("=====================")
 
-    counts = Counter(spinned_items)
-    for count in counts.values():
-        if count == 3:
-            print("🎊YOU WIN!!! $100 were added to your balance!🎊")
-            return 100
-    return -1
+def get_payout(spinned_items, bet):
+    counts = Counter(spinned_items).values()
+    if 3 in counts:
+        print(f"🎊YOU GOT 3 PERFECT MATCH!!! You got ${bet*10}🎊")
+        return bet*10
+    elif 2 in counts:
+        print(f"🎊YOU GOT 2 MATCH!!! You got ${bet*2}🎊")
+        return bet*2
+    else:
+        print("bad luck, try again")
+        return 0
 
 def main():
     print("------ Fruity Slots ------")
     print("symbols: 🍉🍇🍊🍌")
-    print("price: 1$/spin")
     print("--------------------------")
-    balance = int(input("how many balance would you like to start with: $"))
+    balance = 50
 
     while True:
         print(f"current balance: ${balance}")
-        action = input("do you want to spin (y/n): ").lower()
-        if action == "y" or action == "":
-            if balance <= 0:
-                print("cant spin anymore, your officially broke and bankrupt 🥲")
-                break
-            else:
-                time.sleep(0.8)
-                balance += spin()
-        elif action == "n":
-            print("thank you for playing!")
+
+        if balance <=0:
+            print("you cant bet anymore, you're out of balance!")
             break
+        bet = int(input("how much do you want to bet: $"))
+        if bet > balance:
+            print("bet is bigger than current balance, try betting lower")
+        elif bet<=0:
+            print("you cant bet $0 or negative number!")
         else:
-            print("invalid command")
+            balance -= bet
+            print("spinning...\n")
+            time.sleep(0.8)
 
-
-
+            spin_result = spin()
+            display(spin_result)
+            balance += get_payout(spin_result, bet)
 
 if __name__ == '__main__':
     main()
