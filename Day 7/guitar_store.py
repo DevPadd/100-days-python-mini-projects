f9dc018ff757ad5841022615a5479f0b4770cb5a
Dio Naufal Vianto <dionaufalvi@gmail.com>
2025-12-20 23:17:53 +0700

    day 7 commit, kinda lazy so i didnt finish it

---
+++ Day 7/guitar_store.py
@@ -0,0 +1,12 @@
+class Guitar:
+    def __init__(self, name, price, stock):
+        self.name = name
+        self.price = price
+        self.stock = stock
+    def tune():
+        pass
+
+    def play():
+        pass
+
+    
