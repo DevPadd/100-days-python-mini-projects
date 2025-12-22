6d44f97012bc0c0b74906bd4035f587a65f21fb3
Dio Naufal Vianto <dionaufalvi@gmail.com>
2025-12-22 15:02:22 +0700

    added day 9 project

---
+++ day 9 - Alarm/main.py
@@ -0,0 +1,31 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+import pygame
+import datetime
+import time
+
+# pygame initialization
+pygame.mixer.init()
+pygame.mixer.music.load("./ringtone.mp3")
+pygame.mixer.music.set_volume(1)
+
+def set_alarm(alarm_time):
+    print(f"alarm set for {alarm_time}")
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        if current_time < alarm_time:
+            print(current_time)
+        else:
+            print("Wake up!")
+            pygame.mixer.music.play(0)
+            input("press enter to wake the f up and turn off the alarm! ")
+            pygame.mixer.music.stop()
+            break
+        time.sleep(1)
+
+
+
+if __name__ == '__main__':
+    set_alarm(input("Enter alarm time (HH:MM:SS): "))
